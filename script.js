e3bdc5e68622c8e0d81e4db595b61796977846d3
Becode <stevekeersma@gmail.com>
2020-01-10 12:06:09 +0100

    showing weather description

--- script.js
+++ script.js
@@ -2,30 +2,123 @@ let button = document.getElementById("click");
 let input = document.getElementById("cityInput");
 let cityName = document.getElementById("cityname");
 let description = document.getElementById("description");
-let temp = document.getElementById("temperature");
+let tempday1 = document.getElementById("temperature1");
+let tempday2 = document.getElementById("temperature2");
+let tempday3 = document.getElementById("temperature3");
+let tempday4 = document.getElementById("temperature4");
+let tempday5 = document.getElementById("temperature5");
+let descday1 = document.getElementById("description1");
+let descday2 = document.getElementById("description2");
+let descday3 = document.getElementById("description3");
+let descday4 = document.getElementById("description4");
+let descday5 = document.getElementById("description5");
+
 
 
 
 button.addEventListener("click", function () {
 
-   // https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&appid=31a8f4d05ccba8c89d8dcd4ee4fbec1c
-// the ++ allows you to input something inside the string
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=31a8f4d05ccba8c89d8dcd4ee4fbec1c')
-.then(link => link.json())
-.then(data =>  {
-    console.log(data);
-let nameValue = data['name'];
-let kelvValue = data['main']['temp'];
-let descValue = data['weather'][0]['description'];
-let celciusValue = kelvValue-273.15;
-let roundTwoDigits = celciusValue.toFixed(2);
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&appid=31a8f4d05ccba8c89d8dcd4ee4fbec1c')
+        .then(link => link.json())
+        .then(data =>  {
+            let nameValue = data['city']['name'];
+            cityName.innerHTML = nameValue;
+            let infoPerDay = data['list'];
+            let slicer = (start, end)=> {
+                return infoPerDay.slice(start,end)
+            };
+            //slice each day
+            let dayOne = slicer(0,8);
+            let dayTwo = slicer(8,16);
+            let dayThree = slicer(16,24);
+            let dayFour = slicer(24,32);
+            let dayFive = slicer(32,40);
 
-cityName.innerHTML = nameValue;
-temp.innerHTML= roundTwoDigits + '° Celcius';
-description.innerHTML = descValue;
+            //make empty arrays to push temperatures per day
+            let dayOneTemps = [];
+            let dayTwoTemps = [];
+            let dayThreeTemps = [];
+            let dayFourTemps = [];
+            let dayFiveTemps = [];
 
-    })
+            //push temperatures per day
+            for (let i = 0; i < 8 ; i++) {
+                dayOneTemps.push(dayOne[i]['main']['temp']);
+                dayTwoTemps.push(dayTwo[i]['main']['temp']);
+                dayThreeTemps.push(dayThree[i]['main']['temp']);
+                dayFourTemps.push(dayFour[i]['main']['temp']);
+                dayFiveTemps.push(dayFive[i]['main']['temp']);
+            }
+            //make empty arrays to push description
+            let dayOneDescription = [];
+            let dayTwoDescription = [];
+            let dayThreeDescription = [];
+            let dayFourDescription = [];
+            let dayFiveDescription = [];
+            for (let i = 0; i < 8 ; i++) {
+                dayOneDescription.push(dayOne[i]['weather'][0]['description']);
+                dayTwoDescription.push(dayTwo[i]['weather'][0]['description']);
+                dayThreeDescription.push(dayThree[i]['weather'][0]['description']);
+                dayFourDescription.push(dayFour[i]['weather'][0]['description']);
+                dayFiveDescription.push(dayFive[i]['weather'][0]['description'])
+            }
 
-.catch(error => alert("something went wrong"))
+
+
+
+
+
+            //average temperature per day
+            let averageTemp = arr => ((arr.reduce((a,b) => a + b, 0) / arr.length)-273.15).toFixed(2);
+            let tempOne = averageTemp(dayOneTemps);
+            let tempTwo = averageTemp(dayTwoTemps);
+            let tempThree = averageTemp(dayThreeTemps);
+            let tempFour = averageTemp(dayFourTemps);
+            let tempFive = averageTemp(dayFiveTemps);
+
+            //most frequent element in description arrays
+            //used this source for the most frequent function, has very good understandable  structure
+            //https://medium.com/@AmJustSam/how-to-find-most-frequent-item-of-an-array-12015df68c65
+            let counts = {};
+            let compare = 0;
+            let mostFrequent;
+            function frequency(array){
+                for(let i = 0, len = array.length; i < len; i++){
+                    let word = array[i];
+
+                    if(counts[word] === undefined){
+                        counts[word] = 1;
+                    }else{
+                        counts[word] = counts[word] + 1;
+                    }
+                    if(counts[word] > compare){
+                        compare = counts[word];
+                        mostFrequent = array[i];
+                    }
+                }
+                return mostFrequent;
+            }
+
+
+
+            // fill in temperatures
+            tempday1.innerHTML = "temperature today: " +tempOne + " °Celcius";
+            tempday2.innerHTML = "temperature tomorrow: " +tempTwo + " °Celcius";
+            tempday3.innerHTML = "temperature two days from now " +tempThree + " °Celcius";
+            tempday4.innerHTML = "temperature three days from now: " +tempFour + " °Celcius";
+            tempday5.innerHTML = "temperature four days from now: " +tempFive + " °Celcius";
+            console.log(data['list']);
+
+            // fill in descriptions
+            descday1.innerHTML = "weather: " + frequency(dayOneDescription);
+            descday2.innerHTML = "weather: " + frequency(dayTwoDescription);
+            descday3.innerHTML = "weather: " + frequency(dayThreeDescription);
+            descday4.innerHTML = "weather: " + frequency(dayFourDescription);
+            descday5.innerHTML = "weather: " + frequency(dayFiveDescription);
+
+
+        })
+
+        .catch(error => alert("not a city name"))
 
 });
